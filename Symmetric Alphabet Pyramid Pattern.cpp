c6a8a0ef733c16a6d264bdb5ff65b3c6b716fbf9
alokvadera <140166237+alokvadera@users.noreply.github.com>
2025-05-16 09:35:35 +0530

    Create Symmetric Alphabet Pyramid Pattern.cpp

---
+++ Symmetric Alphabet Pyramid Pattern.cpp
@@ -0,0 +1,15 @@
+for (int i = 1; i <= n; i++) {
+for (int j = 1; j <= n - i; j++) {
+cout << " ";
+}
+char ch = 'A';
+for (int j = 1; j <= i; j++) {
+cout << ch++;
+}
+ch = ch - 2;
+while (ch >= 'A') {
+cout << ch;
+ch--;
+}
+cout << endl;
+}
